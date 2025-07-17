bf2f678fcd77e87825a80e2440cd5cd7aba68b7e
kc <kc@gmail.com>
2025-07-17 13:55:48 +0800

    commit

--- public/js/toggleMenu.js
+++
@@ -1,8 +0,0 @@
-const headerNav = document.querySelector(".headerNav ul");
-const toggleMenu = document.querySelector(".toggleMenu");
-
-toggleMenu.addEventListener("click", ()=> {
-    toggleMenu.classList.toggle("open");
-    headerNav.classList.toggle("open");
-});
-
